b7f7152d0fc07a8ce9997044ce50bcb1d5287461
annisafathul <annisafathul18@gmail.com>
2026-04-06 10:47:06 +0700

    mengisi data melalui perulangan

--- app.cpp
+++ app.cpp
@@ -30,5 +30,19 @@ int main()
     cin >> datamu[3];
     cout << "Isikan data kelima : ";
     cin >> datamu[4];
+    
+    // menampilkan data dengan perintah satu persatu
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
+
+    // mengisi data melalui perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Isikan data ke-" << (i + 1) << " : ";
+        cin >> datamu[i];
+    }
 
 }
